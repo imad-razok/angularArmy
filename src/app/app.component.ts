0facee56fb8304afcedb49c8a1938b2c0fbc9f68
unknown <imad.razok.22@gmail.com>
2021-10-10 09:17:04 +0100

    structural Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,55 @@
 import { environment } from './../environments/environment.prod';
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+interface Student {
+  id: number;
+  name: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  @ViewChild('username') myUserName: ElementRef<HTMLInputElement>;
   msg: string = "hello world";
-  someColor: string = "color:#0f0";
-  username: string = "issam";
-  username1: string = "";
+  students: Array<Student>;
+  isActive: boolean = false;
+  colors: Array<string>;
+  selectedColor = '';
+
+
   constructor() {
-    console.log(environment.x);
+    this.students = [
+      { id: 1, name: 'a' },
+      { id: 2, name: 'b' },
+      { id: 3, name: 'c' },
+      { id: 4, name: 'd' },
+      { id: 5, name: 'e' },
+    ];
+    this.colors = [
+      'red', 'blue', 'pink'
+    ];
+
+    setTimeout(() => {
+      console.log(this.myUserName.nativeElement.value);
+    }, 4000);
   }
-  doSomeThing() {
-    console.log('some thing to do');
+  refresh() {
+    this.students = [
+      { id: 1, name: 'a' },
+      { id: 2, name: 'b' },
+      { id: 9, name: 'c' },
+      { id: 4, name: 'd' },
+      { id: 5, name: 'e' },
+    ];
   }
 
+  track(index: number, student: Student) {
+    return student.id;
+  }
+
+  call(phone: number) {
+    console.log(phone)
+  }
 }
