e897cbc9c29d9c38d3a4082cfdf6058517cb6873
unknown <imad.razok.22@gmail.com>
2021-10-10 13:36:46 +0100

    viewChildren

--- src/app/component/parent/parent.component.ts
+++ src/app/component/parent/parent.component.ts
@@ -1,42 +1,18 @@
 import { ChildComponent } from './../child/child.component';
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.scss'],
 })
-export class ParentComponent implements OnChanges, OnInit, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+export class ParentComponent implements AfterViewInit {
 
   myTitle: string = 'my title';
   @ViewChild(ChildComponent, { static: true }) myChildComponent: ChildComponent;
-
-  constructor() {
-    console.log(this.myChildComponent);
-  }
-  ngOnDestroy(): void {
-    console.log(this.myChildComponent);
-  }
-  ngAfterViewChecked(): void {
-    console.log(this.myChildComponent);
-  }
+  @ViewChildren(ChildComponent) childs: QueryList<ChildComponent>;
   ngAfterViewInit(): void {
-    console.log(this.myChildComponent);
-  }
-  ngAfterContentChecked(): void {
-    console.log(this.myChildComponent);
-  }
-  ngAfterContentInit(): void {
-    console.log(this.myChildComponent);
+    console.log(this.childs);
   }
 
-  /*ngDoCheck(): void {
-    console.log('do check')
-  }*/
-  ngOnInit(): void {
-    console.log(this.myChildComponent);
-  }
-  ngOnChanges(changes: SimpleChanges): void {
-    //console.log('change is ', changes);
-  }
 }
