432d0263f48fe00a68077aa1107eb707f305940b
unknown <imad.razok.22@gmail.com>
2021-10-10 13:21:12 +0100

    viewChild

--- src/app/component/parent/parent.component.ts
+++ src/app/component/parent/parent.component.ts
@@ -1,16 +1,42 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ChildComponent } from './../child/child.component';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
-export class ParentComponent implements OnInit {
-  myTitle: string = 'parent title 1'
-  constructor() { }
+export class ParentComponent implements OnChanges, OnInit, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
-  ngOnInit(): void {
+  myTitle: string = 'my title';
+  @ViewChild(ChildComponent, { static: true }) myChildComponent: ChildComponent;
+
+  constructor() {
+    console.log(this.myChildComponent);
+  }
+  ngOnDestroy(): void {
+    console.log(this.myChildComponent);
+  }
+  ngAfterViewChecked(): void {
+    console.log(this.myChildComponent);
+  }
+  ngAfterViewInit(): void {
+    console.log(this.myChildComponent);
+  }
+  ngAfterContentChecked(): void {
+    console.log(this.myChildComponent);
+  }
+  ngAfterContentInit(): void {
+    console.log(this.myChildComponent);
   }
 
+  /*ngDoCheck(): void {
+    console.log('do check')
+  }*/
+  ngOnInit(): void {
+    console.log(this.myChildComponent);
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    //console.log('change is ', changes);
+  }
 }
