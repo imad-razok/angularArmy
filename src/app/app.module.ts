394eac5c6bdf05aa4a20e385986c609eae65207e
unknown <imad.razok.22@gmail.com>
2021-10-21 23:04:40 +0100

    clean for sherd module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,11 @@ import { registerLocaleData } from '@angular/common';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import localeAr from '@angular/common/locales/ar';
-import { FirstComponentComponent } from './first-component/first-component.component';
-import { SecoundComponentComponent } from './secound-component/secound-component.component';
-import { ThirdComponentComponent } from './third-component/third-component.component';
-import { FourthComponentComponent } from './fourth-component/fourth-component.component';
-import { FivethComponentComponent } from './fiveth-component/fiveth-component.component';
-import { SixthComponentComponent } from './sixth-component/sixth-component.component';
 
 registerLocaleData(localeAr, 'ar');
 @NgModule({
   declarations: [
-    AppComponent,
-    FirstComponentComponent,
-    SecoundComponentComponent,
-    ThirdComponentComponent,
-    FourthComponentComponent,
-    FivethComponentComponent,
-    SixthComponentComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
